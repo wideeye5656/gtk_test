f34ebfc268f6ab7d9e03bc6b769f50aa43654c7a
wideeye <wideeye56@gmail.com>
2023-04-24 22:37:34 +0900

    1. 초기 업로드

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+#![windows_subsystem = "windows"]
+
+use gtk::prelude::*;
+use gtk::Orientation;
+use gtk::{glib, Application, ApplicationWindow, Button};
+use std::cell::Cell;
+use std::rc::Rc;
+
+const APP_ID: &str = "MemoryManagement_Sample";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+
+    app.connect_activate(build_ui);
+
+    app.run()
+}
+
+fn build_ui(application:&Application) {
+
+    let button_increase = Button::builder()
+        .label("Increase")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    let button_decrease = Button::builder()
+        .label("Decrease")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    let number = Rc::new(Cell::new(0));
+
+    let number_copy = number.clone();
+    button_increase.connect_clicked(move |_| number_copy.set(number_copy.get() + 1));
+    button_decrease.connect_clicked(move |_| number.set(number.get() - 1));
+
+    let gtk_box = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+
+    gtk_box.append(&button_increase);
+    gtk_box.append(&button_decrease);
+
+    let window = ApplicationWindow::builder()
+        .application(application)
+        .title("MemoryManager Sample")
+        .child(&gtk_box)
+        .build();
+    // let button = Button::builder().label("Press Me!").margin_top(12).margin_bottom(12).margin_start(12).margin_end(12).build();
+
+    // button.connect_clicked(|button| {
+    //     button.set_label("Hello World!");
+    // });
+
+    // let window = ApplicationWindow::builder().application(app).title("My GTK App").child(&button).build();    
+
+     window.present();
+}
